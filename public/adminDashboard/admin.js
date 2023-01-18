036fa3ed690efd17ed1c89321ee11487c67564bf
unknown <manu9g@gmail.com>
2023-01-19 00:40:06 +0530

    new

--- public/adminDashboard/admin.js
+++ public/adminDashboard/admin.js
@@ -29,62 +29,6 @@ async function addBook() {
     alert("Error Adding Product")
   }
 }
-// viewButton.addEventListener('click',viewAndEdit)
 
-fetch('/books')
-  .then(response => response.json())
-  .then(data => {
-    // Create the table element
-    const table = document.createElement('table');
-
-    // Create the table header
-    const headerRow = document.createElement('tr');
-    const headerName = document.createElement('th');
-    headerName.textContent = "Name";
-    const headerActions = document.createElement('th');
-    headerActions.textContent = "Actions";
-    headerRow.appendChild(headerName);
-    headerRow.appendChild(headerActions);
-    table.appendChild(headerRow);
-
-    // Loop through the data and create rows for the table
-    data.forEach(item => {
-      const row = document.createElement('tr');
-      const nameCell = document.createElement('td');
-      const actionsCell = document.createElement('td');
-
-      const updateButton = document.createElement('button');
-      const deleteButton = document.createElement('button');
-      const viewButton = document.createElement('button');
-      
-      nameCell.textContent = item.name;
-      updateButton.textContent = 'Update';
-      deleteButton.textContent = 'Delete';
-      viewButton.textContent = 'View';
-
-      actionsCell.appendChild(updateButton);
-      actionsCell.appendChild(deleteButton);
-      actionsCell.appendChild(viewButton);
-
-      row.appendChild(nameCell);
-      row.appendChild(actionsCell);
-
-      // Attach event listeners to the buttons
-      updateButton.addEventListener('click', () => {
-        // Handle update logic
-      });
-      deleteButton.addEventListener('click', () => {
-        // Handle delete logic
-      });
-      viewButton.addEventListener('click', () => {
-        // Handle view logic
-      });
-
-      table.appendChild(row);
-    });
-    // Append the table to the HTML
-    document.body.appendChild(table);
-  })
-  .catch(error => console.error(error));
 
 
